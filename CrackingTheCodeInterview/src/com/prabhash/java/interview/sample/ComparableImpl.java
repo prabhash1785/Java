b7ded10ee552dd7800bd6775008b897af25e2d0d
Prabhash Rathore <prabhashrathore@gmail.com>
2015-03-20 00:20:26 -0700

    sort a list of object

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/sample/ComparableImpl.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/sample/ComparableImpl.java
@@ -1,5 +1,9 @@
 package com.prabhash.java.interview.sample;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * This class will implement Comparable interface to compare two objects.
  * 
@@ -10,10 +14,30 @@ public class ComparableImpl {
 	
 	private static Employee e1 = new Employee(25, "Peter");
 	private static Employee e2 = new Employee(10, "Russel");
+	
+	private static List<Employee> empList = new ArrayList<Employee>();  
 
 	public static void main(String[] args) {
 		
 		System.out.println("Is e1 > e2: " + e1.compareTo(e2));
+		
+		empList.add(e1);
+		empList.add(e2);
+		empList.add(new Employee(34, "Mango"));
+		empList.add(new Employee(4, "Apple"));
+		empList.add(new Employee(12, "Honda"));
+		
+		System.out.println("List before sorting: ");
+		for(Employee e : empList) {
+			System.out.println(e);
+		}
+		
+		//sort the list
+		Collections.sort(empList);
+		System.out.println("List after sorting: ");
+		for(Employee e : empList) {
+			System.out.println(e);
+		}
 
 	}
 	
@@ -42,6 +66,11 @@ public class ComparableImpl {
 				return 0;
 			}
 			
+		}
+		
+		@Override
+		public String toString() {
+			return this.empNum + " :: " + this.empName;
 		}
 		
 	}
