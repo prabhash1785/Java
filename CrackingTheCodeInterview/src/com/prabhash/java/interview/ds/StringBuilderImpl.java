2406523a74e58881c0c74cfd23b26add1b78f0b0
Prabhash Rathore <prabhashrathore@gmail.com>
2015-03-22 00:51:56 -0700

    StringBuffer implementation

---
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ds/StringBuilderImpl.java
@@ -0,0 +1,75 @@
+package com.prabhash.java.interview.ds;
+
+/**
+ * Implementtaion of StringBuffer Data Strcuture.
+ * 
+ * @author prrathore
+ *
+ */
+public class StringBuilderImpl {
+	
+	private char[] buffer;
+	private int capacity;
+	private int length;
+
+	public StringBuilderImpl() {
+		this(16);
+	}
+
+	public StringBuilderImpl(int n) {
+		buffer = new char[n];
+		capacity = n;
+		length = 0;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+
+	public void ensureSize(int newLength) {
+		if(newLength < 0) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		int k = 2 * capacity;
+		char[] dest = new char[k];
+		
+		//copy existing array to new array
+		System.arraycopy(buffer, 0, dest, 0, length);
+		buffer = dest; //point buffer to new array
+		capacity = k; //update capacity
+	}
+
+	public StringBuilderImpl append(String s) {
+		if(s == null) {
+			return this;
+		}
+		int newLength = this.length + s.length();
+		if(newLength > capacity) {
+			ensureSize(newLength);
+		}
+		//copy the string to existing array of characters
+		for(int i = 0; i < s.length(); i++, length++) {
+			buffer[length] = s.charAt(i);
+		}
+		
+		return this;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return new String(buffer, 0, length);
+	}
+	
+	public static void main(String[] args) {
+		
+		StringBuilderImpl sb = new StringBuilderImpl();
+		sb.append("America is Aweosme.").append(" India is aweosme too.").append(" This is a great stringbuffer program.");
+		
+		System.out.println("StringBuffer: " + sb);
+		System.out.println("Size: " + sb.length + " :::: Capacity: " + sb.capacity);
+		
+	}
+
+
+}
