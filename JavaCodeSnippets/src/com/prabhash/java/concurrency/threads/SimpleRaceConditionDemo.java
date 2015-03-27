82077a1449ca38c6808d87f072e53f412391976e
Prabhash Rathore <prabhashrathore@gmail.com>
2015-03-27 00:58:54 -0700

    Race condition between 2 threads

---
+++ JavaCodeSnippets/src/com/prabhash/java/concurrency/threads/SimpleRaceConditionDemo.java
@@ -0,0 +1,59 @@
+package com.prabhash.java.concurrency.threads;
+
+/**
+ * Program to create Race Condition between two threads in a Critical Section which is not guarded by Synchronized keyword.
+ * 
+ * @author prrathore
+ *
+ */
+public class SimpleRaceConditionDemo {
+	
+	private int count = 0;
+	
+	/**
+	 * This method is the Critical Section in the program as it's changing the state of this class without having any synchronization
+	 * so this could lead to Race Condition between two threads and the result depends on the order in which thread runs this method.
+	 * 
+	 * This critical section will lead to Race Condition.
+	 * 
+	 */
+	public void add(int n) {
+		this.count += n;
+	}
+
+	public static void main(String[] args) {
+		
+		final SimpleRaceConditionDemo obj = new SimpleRaceConditionDemo();
+		
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				obj.add(5);
+			}
+		}, "Thread1") ;
+		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				obj.add(10);
+			}
+		}, "Thread2") ;
+		
+		t1.start();
+		
+//		try {
+//			Thread.sleep(1);
+//		} catch(InterruptedException i) {
+//			System.out.println("Interrupted!!");
+//		}
+		
+		t2.start();
+		
+		// in debug mode, this statement executes before other threads runs runnable tasks because this statement is being run by 
+		// main thread which could run in parallel with other threads
+		// Based on Thread Race Condition, the output could be -> 0, 10, 15
+		System.out.println("Value of count is: " + obj.count); 
+
+	}
+
+}
