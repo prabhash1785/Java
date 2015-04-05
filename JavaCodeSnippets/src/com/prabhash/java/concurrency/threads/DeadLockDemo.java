c9c5b66a23046cbaaa58cbc4fc9206d06dd6d263
Prabhash Rathore <prabhashrathore@gmail.com>
2015-04-04 23:53:15 -0700

    program to create a deadlock

--- JavaCodeSnippets/src/com/prabhash/java/concurrency/threads/DeadLockDemo.java
+++ JavaCodeSnippets/src/com/prabhash/java/concurrency/threads/DeadLockDemo.java
@@ -10,10 +10,29 @@ public class DeadLockDemo {
 	
 	public synchronized void method1() {
 		System.out.println(Thread.currentThread().getName() + " => Method 1");
+		try {
+			Thread.sleep(5000);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public synchronized void method2() {
 		System.out.println(Thread.currentThread().getName() + " => Method 2");
+		try {
+			Thread.sleep(5000);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized void method3() {
+		System.out.println("Method3!!");
+	}
+	
+	public synchronized void method4() {
+		System.out.println("Method4!!");
 	}
 
 	public static void main(String[] args) {
@@ -25,13 +44,15 @@ public class DeadLockDemo {
 			@Override
 			public void run() {
 				monitorObj1.method1();
+				monitorObj2.method3();
 			}
 		});
 		
 		Thread thread2 = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				monitorObj2.method2();;
+				monitorObj2.method2();
+				monitorObj1.method4();
 			}
 		});
 		
