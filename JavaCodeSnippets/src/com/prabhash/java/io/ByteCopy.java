6274f3255ff6b1fa8808090d5f94c6a6bf594e4a
Prabhash Rathore <prabhashrathore@gmail.com>
2015-04-15 23:32:49 -0700

    Copy bytes from one file to other using byte stream

---
+++ JavaCodeSnippets/src/com/prabhash/java/io/ByteCopy.java
@@ -0,0 +1,46 @@
+package com.prabhash.java.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Basic program to copy bytes from one file to another file. This program will use FileInputStream class to read a file and write using
+ * FileOutput stream.
+ * 
+ * I understand copying a text file can be done using a higher level stream class like a FileReader but this is just a practice program
+ * to write some code using ByteStream.
+ * 
+ * @author prrathore
+ *
+ */
+public class ByteCopy {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		
+		try {
+			in = new FileInputStream("./data/data.txt");
+			out = new FileOutputStream("./data/output.txt"); //this will create a file if file doesn't exist
+			
+			int c;
+			
+			while((c = in.read()) != -1) {
+				out.write(c);
+			}
+			
+		} finally {
+			if(in != null) {
+				in.close();
+			}
+			if(out != null) {
+				out.close();
+			}
+		}
+		
+
+	}
+
+}
