8dd86bd2163923bf538ddd2c729bbdd8af344a00
Prabhash Rathore <prabhashrathore@gmail.com>
2015-04-19 09:12:43 -0700

    Object Deserialization using ObjectInputStream

---
+++ JavaCodeSnippets/src/com/prabhash/java/io/Deserialization.java
@@ -0,0 +1,51 @@
+package com.prabhash.java.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/**
+ * Deserialization of an object using ObjectInputStream class.
+ * 
+ * Issues faced while deserialization:
+ * 	- Without having a serialVersionUID in class of serialized object, I couldn' deserialize the object. The exception thrown was InvalidClassException.
+ *    After adding SerialVersionUID, I was able to deserialize the object.
+ * 
+ * @author prrathore
+ *
+ */
+public class Deserialization {
+
+	public static void main(String[] args) throws IOException {
+		
+		ObjectInputStream objectInputStream = null;
+		FileInputStream fileInputStream = null;
+		
+		try {
+			
+			fileInputStream = new FileInputStream("./data/SerializedContent.temp");
+			objectInputStream = new ObjectInputStream(fileInputStream);
+			
+			ObjectStreams obj = (ObjectStreams)objectInputStream.readObject(); //this deserialized object is a new object with same fields and values as original object
+			
+			System.out.println("First Name: " + obj.getFirstName());
+			System.out.println("Last Name: " + obj.getLastName());
+			System.out.println("Emp Num: " + obj.getEmpNum());
+			
+			
+		} catch(IOException io) {
+			io.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(fileInputStream != null) {
+				fileInputStream.close();
+			}
+			if(objectInputStream != null) {
+				objectInputStream.close();
+			}
+		}
+
+	}
+
+}
--- JavaCodeSnippets/src/com/prabhash/java/io/ObjectStreams.java
+++ JavaCodeSnippets/src/com/prabhash/java/io/ObjectStreams.java
@@ -14,10 +14,16 @@ import java.io.Serializable;
  * Other uses include passing objects between hosts using a socket stream or for marshaling and unmarshaling arguments and parameters 
  * in a remote communication system.
  * 
+ * Serialization:
+ *  - If a class to be serialized is not defined Serializable then this can't be serialized. While serializing, you will get 
+ *  NotSerilizableException
+ * 
  * @author prrathore
  *
  */
 public class ObjectStreams implements Serializable {
+	
+	private static final long serialVersionUID = 20L;
 	
 	private String firstName;
 	private String lastName;
@@ -29,22 +35,47 @@ public class ObjectStreams implements Serializable {
 		this.empNum = empNum;
 	}
 	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getEmpNum() {
+		return empNum;
+	}
+
+	public void setEmpNum(int empNum) {
+		this.empNum = empNum;
+	}
+
 	// Write instance of class ObjectStreams to a file stream and then read it back from the file stream. 
 	public static void main(String[] args) throws IOException {
 		
 		ObjectStreams obj = new ObjectStreams("Ricky", "Rathore", 100);
 		
+		//fileoutputstream to write to a file using byte stream
 		FileOutputStream fileOutputStream = new FileOutputStream("./data/SerializedContent.temp");
 		
+		//create ObjectOutputStream to serialize an object
 		ObjectOutputStream objStream = new ObjectOutputStream(fileOutputStream);
 		
+		//serialize the object
 		objStream.writeObject(obj);
 		
 		objStream.close();
 		fileOutputStream.close();
 		
-		
-
 	}
 
 }
