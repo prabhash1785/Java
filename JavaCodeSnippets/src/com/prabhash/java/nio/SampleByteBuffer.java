bf3949ff380279cf5bb4f706759001ec95143d88
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-04 23:18:00 -0700

    Use buffer and channel to read from a  file

--- JavaCodeSnippets/src/com/prabhash/java/nio/SampleByteBuffer.java
+++ JavaCodeSnippets/src/com/prabhash/java/nio/SampleByteBuffer.java
@@ -1,5 +1,11 @@
 package com.prabhash.java.nio;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * A sample byte buffer.
  * 
@@ -9,8 +15,36 @@ package com.prabhash.java.nio;
 public class SampleByteBuffer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		RandomAccessFile file;
+		
+		try {
+			file = new RandomAccessFile("./data/data.txt", "rw");
+			
+			FileChannel fileChannel = file.getChannel();
+			
+			ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+			
+			int bytesRead = fileChannel.read(byteBuffer);
+			
+			while(bytesRead != -1) {
+				byteBuffer.flip(); //flip it for read from write
+				
+				while(byteBuffer.hasRemaining()) {
+					System.out.println((char) byteBuffer.get());
+				}
+				
+				byteBuffer.clear();
+				bytesRead = fileChannel.read(byteBuffer);
+				
+			}
+			
+		} catch(FileNotFoundException f) {
+			f.printStackTrace();
+		} catch(IOException io) {
+			io.printStackTrace();
+		}
+		
 	}
 
 }
