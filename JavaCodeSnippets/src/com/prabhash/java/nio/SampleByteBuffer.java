cbaecd587015798e64461af9e23b5ba8dba687b8
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-04 23:02:41 -0700

    skeleton of a basic byte buffer

---
+++ JavaCodeSnippets/src/com/prabhash/java/nio/SampleByteBuffer.java
@@ -0,0 +1,16 @@
+package com.prabhash.java.nio;
+
+/**
+ * A sample byte buffer.
+ * 
+ * @author prrathore
+ *
+ */
+public class SampleByteBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
