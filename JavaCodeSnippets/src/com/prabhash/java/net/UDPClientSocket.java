7d0059a7b0960a331fc4f858177b363e3d64541c
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-06 23:49:17 -0700

    class name refactoring

--- JavaCodeSnippets/src/com/prabhash/java/net/UDPClientSocket.java
+++ JavaCodeSnippets/src/com/prabhash/java/net/UDPClientSocket.java
@@ -10,7 +10,7 @@ import java.net.InetAddress;
  * @author prrathore
  *
  */
-public class UDPServerSocket {
+public class UDPClientSocket {
 
 	public static void main(String[] args) throws Exception {
 		
