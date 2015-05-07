ec05fd8c61f20d4e39b66fc51dd46546bb47bc03
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-06 23:42:44 -0700

    UDP Socket implementation

---
+++ JavaCodeSnippets/src/com/prabhash/java/net/UDPServerSocket.java
@@ -0,0 +1,30 @@
+package com.prabhash.java.net;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * UDP Server Socket
+ * 
+ * @author prrathore
+ *
+ */
+public class UDPServerSocket {
+
+	public static void main(String[] args) throws Exception {
+		
+		byte[] data = "123456789".getBytes();
+		
+		InetAddress address = InetAddress.getLocalHost();
+		
+		DatagramPacket packet = new DatagramPacket(data, data.length, address, 9000);
+		
+		DatagramSocket datagramSocket = new DatagramSocket();
+		datagramSocket.send(packet);
+		
+		datagramSocket.close();
+
+	}
+
+}
