6a72b54c2c38b71f36627d92bb211157357b3978
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-10 00:18:34 -0700

    Get server content using URL connection class methods

---
+++ JavaCodeSnippets/src/com/prabhash/java/net/URLConnectionDemo.java
@@ -0,0 +1,41 @@
+package com.prabhash.java.net;
+
+import java.net.*;
+import java.io.*;
+
+public class URLConnectionDemo {
+	
+   public static void main(String [] args)
+   {
+      try
+      {
+         URL url = new URL("https://paypal.com");
+         
+         URLConnection urlConnection = url.openConnection();
+         
+         HttpURLConnection connection = null;
+         
+         if(urlConnection instanceof HttpURLConnection) {
+            connection = (HttpURLConnection) urlConnection;
+         } else {
+            System.out.println("Please enter an HTTP URL.");
+            return;
+         }
+         
+         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+         
+         String urlString = "";
+         String current;
+         
+         while((current = in.readLine()) != null) {
+            urlString += current;
+         }
+         
+         System.out.println(urlString);
+         
+      } catch(IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+}
