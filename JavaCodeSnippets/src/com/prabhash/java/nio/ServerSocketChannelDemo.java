c3440240bca80fcddd9d9256720adbb47cd642c6
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-10 08:34:35 -0700

    skeleton of a server socket channel class

---
+++ JavaCodeSnippets/src/com/prabhash/java/nio/ServerSocketChannelDemo.java
@@ -0,0 +1,16 @@
+package com.prabhash.java.nio;
+
+/**
+ * This class will listen for client connections and print the messages sent by clients.
+ * 
+ * @author prrathore
+ *
+ */
+public class ServerSocketChannelDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
