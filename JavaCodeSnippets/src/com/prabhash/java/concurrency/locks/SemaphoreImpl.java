24cea10c2a53039af49df0f7f954edbd50b69e56
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-13 07:29:53 -0700

    constructor definition for semaphore

--- JavaCodeSnippets/src/com/prabhash/java/concurrency/locks/SemaphoreImpl.java
+++ JavaCodeSnippets/src/com/prabhash/java/concurrency/locks/SemaphoreImpl.java
@@ -1,5 +1,7 @@
 package com.prabhash.java.concurrency.locks;
 
+import java.security.InvalidParameterException;
+
 /**
  * Semaphore implementation.
  * 
@@ -7,5 +9,23 @@ package com.prabhash.java.concurrency.locks;
  *
  */
 public class SemaphoreImpl {
+	
+	private final boolean[] semaphore;
+	private final int capacity;
+	
+	public SemaphoreImpl() {
+		this(5);
+	}
+	
+	public SemaphoreImpl(int capacity) {
+		if(capacity <= 0 || capacity > 10) {
+			throw new InvalidParameterException("Invalid value of Semaphore capacity, it has to be between 1 and 10!!");
+		}
+		
+		this.capacity = capacity;
+		this.semaphore = new boolean[capacity];
+	}
+	
+	
 
 }
