b60cd4e965bed7d836e768f0094e6091af81411e
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-13 23:11:35 -0700

    Blocking Queue implementation

---
+++ JavaCodeSnippets/src/com/prabhash/java/concurrency/locks/BlockingQueueImpl.java
@@ -0,0 +1,60 @@
+package com.prabhash.java.concurrency.locks;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Blocking Queue implementation which will block to add element if queue is full or block to remove element if queue is empty.
+ * 
+ * @author prrathore
+ *
+ */
+public class BlockingQueueImpl {
+	
+	private List<String> queue = new LinkedList<String>();
+	private int limit;
+	
+	public BlockingQueueImpl() {
+		this(10);
+	}
+	
+	public BlockingQueueImpl(int limit) {
+		this.limit = limit;
+	}
+	
+	/**
+	 * Add element to queue. This will block if queue is full.
+	 * 
+	 */
+	public synchronized void add(String element) throws InterruptedException {
+		
+		while(queue.size() == limit) {
+			wait();
+		}
+		
+		if(queue.size() == 0) {
+			notifyAll();
+		}
+		
+		queue.add(element);
+		
+	}
+	
+	/**
+	 * Remove head of the queue. If queue is empty then this will block.
+	 * 
+	 */
+	public synchronized String remove() throws InterruptedException {
+		
+		if(queue.size() == 0) {
+			wait();
+		}
+		
+		if(queue.size() == limit) {
+			notifyAll();
+		}
+		
+		return queue.remove(0);
+	}	
+
+}
