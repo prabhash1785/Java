497c6202601ed6d24a12fe2552448c8a9a4ade6c
Prabhash Rathore <prabhashrathore@gmail.com>
2015-07-25 20:38:17 -0700

    getters and setters for nested Node class

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/LinkedListImpl.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/LinkedListImpl.java
@@ -228,6 +228,22 @@ public class LinkedListImpl {
 		public Node(int data) {
 			this.data = data;
 		}
+
+		public int getData() {
+			return data;
+		}
+
+		public void setData(int data) {
+			this.data = data;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
 		
 	}
 
