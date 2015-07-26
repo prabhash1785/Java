6cdda55456025e9e0a7bce820be320049a450782
Prabhash Rathore <prabhashrathore@gmail.com>
2015-07-25 20:30:09 -0700

    initial skeleton to generate a Linked List and get head reference

---
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/Q2_1_RemoveDuplicateFromLL.java
@@ -0,0 +1,25 @@
+package com.prabhash.java.interview.ch2;
+
+/**
+ * Q 2.1
+ * 
+ * Remove duplicate from Linked List
+ * 
+ * @author prrathore
+ *
+ */
+public class Q2_1_RemoveDuplicateFromLL {
+	
+	
+
+	public static void main(String[] args) {
+		
+		final LinkedListImpl ll = new LinkedListImpl();
+		
+		ll.generateLinkedList(); // generate a Linked List
+		
+		LinkedListImpl.Node head = ll.getHead(); //get a reference to Linked List head
+		
+	}
+
+}
