f7ce8b92a295148f554bed914dd36fb4d9afdc4d
Prabhash Rathore <prabhashrathore@gmail.com>
2015-07-26 23:15:23 -0700

    method to find kth node from last

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/FindNthToLastNode.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/FindNthToLastNode.java
@@ -7,6 +7,46 @@ package com.prabhash.java.interview.ch2;
  *
  */
 public class FindNthToLastNode {
+	
+	/**
+	 * Using two pointers with a difference of k nodes between them, find the required node once the faster node hits the last node.
+	 * 
+	 * @param node
+	 * @param k
+	 * @return data
+	 */
+	public static int findKthToLast(LinkedListImpl.Node node, int k) {
+		
+		if(node == null) {
+			throw new NullPointerException("Linked List is null");
+		}
+		
+		LinkedListImpl.Node prev = node;
+		
+		int counter = 0; //counter to keep track of ditance between prev and next nodes
+		
+		while(node != null) {
+			
+			if(counter < k) {
+				counter++;
+			} else {
+				prev = prev.getNext();
+			}
+			
+			node = node.getNext();
+			
+		}
+		
+		if(counter < k) {
+			System.out.println("There are not enough nodes in Linked List to find kth to last node.");
+			return -1;
+		}
+		
+		int data = prev.getData();
+		
+		return data;
+		
+	}
 
 	public static void main(String[] args) {
 		
@@ -17,6 +57,9 @@ public class FindNthToLastNode {
 		LinkedListImpl.Node head = ll.getHead();
 		
 		ll.prettyPrintLinkedList(head);
+		
+		int data = findKthToLast(head, 2);
+		System.out.println("\n\nKth element from last: " + data);
 
 	}
 
