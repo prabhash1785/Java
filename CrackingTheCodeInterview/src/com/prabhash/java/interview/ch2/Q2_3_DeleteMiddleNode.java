395430008937800b731142670c1e1de77536081d
Prabhash Rathore <prabhashrathore@gmail.com>
2015-07-28 23:11:57 -0700

    initial structure of algorithm to delete middle node

---
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/Q2_3_DeleteMiddleNode.java
@@ -0,0 +1,21 @@
+package com.prabhash.java.interview.ch2;
+
+/**
+ * Delete the middle node of a Linked List, given you have access to only that node.
+ * 
+ * @author prrathore
+ *
+ */
+public class Q2_3_DeleteMiddleNode {
+
+	
+	
+	public static void main(String[] args) {
+		
+		LinkedListImpl ll = new LinkedListImpl();
+		
+		ll.generateLinkedList();
+
+	}
+
+}
