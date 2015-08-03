36537f45a4a80ba679d5b5b0d992f147b2d04d96
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-03 08:37:47 -0700

    determine the start of loop in a Linked List

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/Q2_6_DetermineLoopinLinkedList.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/Q2_6_DetermineLoopinLinkedList.java
@@ -9,6 +9,48 @@ package com.prabhash.java.interview.ch2;
 public class Q2_6_DetermineLoopinLinkedList {
 	
 	/**
+	 * Find starting node of the Loop.
+	 * 
+	 * Solution:
+	 * Keep two nodes - fast and slow. Fast will move two steps and slow will move one step.
+	 * They will collide at some point.
+	 * Move one node back to head and then keep moving both one step at a time. They will collide again at the start of the loop. Return
+	 * this node.
+	 * 
+	 */
+	public static LinkedListImpl.Node determineStartOfLoop(LinkedListImpl.Node node) {
+		
+		LinkedListImpl.Node fast = node;
+		LinkedListImpl.Node slow = node;
+		
+		while(fast != null && fast.getNext() != null) {
+			slow = slow.getNext();
+			fast = fast.getNext().getNext();
+			
+			if(slow == fast) { //loop determined
+				break;
+			}
+		}
+		
+		// if above loop resulted in fast or fast.next being null then there is no loop
+		if(fast == null || fast.getNext() == null) {
+			return null;
+		}
+		
+		slow = node; //move slow back to head
+		
+		// Now move slow and fast one step each, they will collide again at the start of the loop
+		while(slow != fast) {
+			slow = slow.getNext();
+			fast = fast.getNext();
+		}
+		
+		return fast;
+		
+	}
+	
+	
+	/**
 	 * Determine if Linked List has a loop.
 	 * 
 	 * Have two pointers - one fast and slow. Move fast twice as fast as slow. If there is a loop then they should collide else they will
@@ -79,7 +121,15 @@ public class Q2_6_DetermineLoopinLinkedList {
 		
 		boolean hasLoop = hasLoop(head);
 		System.out.println("\n\nLinked List has loop: " + hasLoop);
-
+		
+		LinkedListImpl.Node startOfLoop = determineStartOfLoop(head);
+		
+		if(startOfLoop == null) {
+			System.out.println("\n\nThere was no loop in Linked List");		
+		} else {
+			System.out.println("\n\nStart of Loop is: " + startOfLoop.getData());
+		}
+		
 	}
 
 }
