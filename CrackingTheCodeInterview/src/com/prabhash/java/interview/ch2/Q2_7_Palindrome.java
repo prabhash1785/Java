046888f5f39b8f09e93085d508dc6978e05c829a
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-06 08:22:27 -0700

    Optimized algorithm to push first half of Linked List to stack and then comparing remaining nodes to data in stack

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/Q2_7_Palindrome.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch2/Q2_7_Palindrome.java
@@ -67,13 +67,43 @@ public class Q2_7_Palindrome {
 			throw new NullPointerException("Linked List is null");
 		}
 		
+		Stack<Integer> stack = new Stack<Integer>();
 		
+		// run two pointers, one fast and other slow to find the middle node of Linked List and push nodes upto middle node to stack
+		LinkedListImpl.Node fast = node;
+		LinkedListImpl.Node slow = node;
+		
+		while(fast != null && fast.getNext() != null) {
+			
+			stack.push(slow.getData());
+			
+			slow = slow.getNext();
+			fast = fast.getNext().getNext();
+			
+		}
+		
+		// for odd number of nodes, fast must not be null so move slow node one position over to right so that we don't have to compare this middle node 
+		// with nodes on right
+		if(fast != null) {
+			slow = slow.getNext();
+		}
+		
+		while(slow != null) {
+			int listData = slow.getData();
+			int stackData = stack.pop();
+			
+			if(listData != stackData) {
+				return false;
+			}
+			
+			slow = slow.getNext();
+		}
 		
 		return true;
 	}
 	
 	/**
-	 * Find middle node of a Linked List
+	 * Find middle node of a Linked List iteratively
 	 * 
 	 * @param node
 	 * @return
@@ -105,7 +135,7 @@ public class Q2_7_Palindrome {
 		ll.addNode(8);
 		ll.addNode(23);
 		ll.addNode(7);
-		ll.addNode(7);
+		//ll.addNode(7);
 		ll.addNode(23);
 		ll.addNode(8);
 		ll.addNode(4);
@@ -113,9 +143,13 @@ public class Q2_7_Palindrome {
 		LinkedListImpl.prettyPrintLinkedList(ll.getHead());
 		
 		boolean isPalindrome = isPalindromeUsingStack(ll.getHead());
-		System.out.println("\n\nIs is palindrome: " + isPalindrome);
+		System.out.println("\n\nIs palindrome: " + isPalindrome);
 		
 		findMiddleOfLinkedList(ll.getHead());
+		
+		boolean isPalindrome2 = isPalindromeUsingStackOptimized(ll.getHead());
+		
+		System.out.println("\n\nIs Palindrome with optimized method: " + isPalindrome2);
 
 	}
 
