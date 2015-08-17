1f310f949407885965a845180da92f843a77c9a7
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-16 21:28:08 -0700

    initial class skeleton

---
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/Q4_4_CreateLinkedListFromEachTreeLevel.java
@@ -0,0 +1,16 @@
+package com.prabhash.java.interview.ch4;
+
+/**
+ * Create Linked Lists from each tree level.
+ * 
+ * @author prrathore
+ *
+ */
+public class Q4_4_CreateLinkedListFromEachTreeLevel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
