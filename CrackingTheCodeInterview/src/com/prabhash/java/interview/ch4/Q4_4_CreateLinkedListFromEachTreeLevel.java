003487def8a995c807e06f8df66255715da87c23
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-17 23:35:35 -0700

    documentation

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/Q4_4_CreateLinkedListFromEachTreeLevel.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/Q4_4_CreateLinkedListFromEachTreeLevel.java
@@ -11,6 +11,13 @@ import java.util.List;
  */
 public class Q4_4_CreateLinkedListFromEachTreeLevel {
 	
+	/**
+	 * Use BFS algorithm to find all the node at each level and create a Linked List of those nodes. Keep adding these Linked List
+	 * to a List which will be returned at the end.
+	 * 
+	 * @param root
+	 * @return
+	 */
 	public static List<List<Integer>> createLinkedListForEachLevel(TreeImpl.Node root) {
 		
 		List<List<Integer>> list = new ArrayList<List<Integer>>();
