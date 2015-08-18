cf1e5dfc4d943ff7230d17ca5da8ac6474bc3556
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-17 23:34:22 -0700

    method signature

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/Q4_4_CreateLinkedListFromEachTreeLevel.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/Q4_4_CreateLinkedListFromEachTreeLevel.java
@@ -1,5 +1,8 @@
 package com.prabhash.java.interview.ch4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Create Linked Lists from each tree level.
  * 
@@ -7,6 +10,15 @@ package com.prabhash.java.interview.ch4;
  *
  */
 public class Q4_4_CreateLinkedListFromEachTreeLevel {
+	
+	public static List<List<Integer>> createLinkedListForEachLevel(TreeImpl.Node root) {
+		
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		
+		
+		return list;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
