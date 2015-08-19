12d702ca23f4a914dcbe1e25f39a2fc1e06c4519
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-19 09:34:12 -0700

    initial class definition to print each path from root to leaf

---
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/PathFromRootToEachLeaf.java
@@ -0,0 +1,16 @@
+package com.prabhash.java.interview.ch4;
+
+/**
+ * Get Path from Root to each leaf in a tree.
+ * 
+ * @author prrathore
+ *
+ */
+public class PathFromRootToEachLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
