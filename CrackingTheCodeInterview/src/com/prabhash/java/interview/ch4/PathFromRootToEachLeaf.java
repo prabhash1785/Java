e5cf4f11b844c07f64ffe12411ce11f796ca3e55
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-20 08:07:07 -0700

    algorithm to print paths from root to leaf recursively

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/PathFromRootToEachLeaf.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/PathFromRootToEachLeaf.java
@@ -1,5 +1,7 @@
 package com.prabhash.java.interview.ch4;
 
+import java.util.List;
+
 /**
  * Get Path from Root to each leaf in a tree.
  * 
@@ -7,9 +9,51 @@ package com.prabhash.java.interview.ch4;
  *
  */
 public class PathFromRootToEachLeaf {
+	
+	public static void getPathFromRootToLeaf(TreeImpl.Node root) {
+		
+		int[] list = new int[20];
+		int count = 0;
+		getPathRecursively(root, list, count);
+		
+	}
+	
+	private static void getPathRecursively(TreeImpl.Node node, int[] list, int index) {
+		
+		if(node == null) {
+			return;
+		}
+		
+		list[index] = node.getKey();
+		
+		index = index + 1;
+		
+		getPathRecursively(node.getLeft(), list, index);
+		getPathRecursively(node.getRight(), list, index);
+		
+		// if encountered a leaf, print the list which is the path from root to leaf
+		if(node.getLeft() == null && node.getRight() == null) {
+			printArray(list, index);
+		}
+			
+	}
+	
+	private static void printArray(int[] list, int count) {
+		
+		for(int i = 0; i < count; i++) {
+			System.out.print(list[i] + " ");
+		}
+		
+		System.out.println("\n");
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		TreeImpl tree = new TreeImpl();
+		
+		tree.generateTree(new int[] {4, 3, 8, 2, 1, 7, 12});
+		
+		getPathFromRootToLeaf(tree.getRoot());
 
 	}
 
