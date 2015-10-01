f8e08527349563c126fd25303da61f3986143f99
Prabhash Rathore <prabhashrathore@gmail.com>
2015-10-01 06:28:04 -0700

    find duplicate number from an array

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/sample/FindRepeatingNumber.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/sample/FindRepeatingNumber.java
@@ -1,27 +1,35 @@
 package com.prabhash.java.interview.sample;
 
-import java.util.Arrays;
-
+/**
+ * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+ *
+ * Note:
+ * You must not modify the array (assume the array is read only).
+ * You must use only constant, O(1) extra space.
+ * Your runtime complexity should be less than O(n2).
+ * There is only one duplicate number in the array, but it could be repeated more than once.
+ *  
+ * @author prrathore
+ *
+ */
 public class FindRepeatingNumber {
 	
-	    public static int findDuplicate(int[] nums) {
-	    	
-	    	Arrays.sort(nums);
-	        
-	        int n = nums[nums.length - 1];
-	        int originalSum = (n * (n + 1)) / 2;
-	        
-	        int sum = 0;
-	        for(int i = 0; i < nums.length; i++) {
-	            sum = sum + nums[i];
-	        }
-	        
-	        int num = sum - originalSum;
-	        
-	        return num;
-	        
+	public static int findDuplicate(int[] nums) {
+		
+	    int n = nums.length; // start at last index
+	    int slow = n;
+	    int fast = n;
+	    do{
+	        slow = nums[slow-1];
+	        fast = nums[nums[fast-1]-1];
+	    }while(slow != fast);
+	    slow = n;
+	    while(slow != fast){
+	        slow = nums[slow-1];
+	        fast = nums[fast-1];
 	    }
-
+	    return slow;
+	}
 
 	public static void main(String[] args) {
 		
