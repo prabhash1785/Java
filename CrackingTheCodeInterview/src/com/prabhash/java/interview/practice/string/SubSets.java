e5dbdf56bcecefdca32a4594ede46cd2674c1c4c
Prabhash Rathore <prabhashrathore@gmail.com>
2016-02-13 08:49:58 -0800

    Powerset using combinatorics method

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/practice/string/SubSets.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/practice/string/SubSets.java
@@ -16,6 +16,14 @@ import java.util.List;
  */
 public class SubSets {
 	
+	/**
+	 * Method 1: Recursive method to find powerset of a string.
+	 * 
+	 * Time Complexity: O(2 ^ n)
+	 * 
+	 * @param s
+	 * @return
+	 */
 	public static List<String> findAllSubSets(String s) {
 		
 		if(s == null || s.length() == 0) {
@@ -60,7 +68,66 @@ public class SubSets {
 		
 	}
 	
-	public static void printList(List<String> list) {
+	/**
+	 * Method 2: Find powerset using Combinatorics method.
+	 * 
+	 * Use binary string from 0 to 1 << n to represent subsets of a string. 
+	 * 
+	 * @param args
+	 */
+	public static List<String> powerSetUsingCombinatorics(String input) {
+		
+		if(input == null || input.length() == 0) {
+			return null;
+		}
+		
+		List<String> powerSet = new ArrayList<>();
+		
+		int inputLength = input.length();
+		int powerSetSize = 1 << inputLength;
+		
+		for(int i = 0; i < powerSetSize; i++) {
+			
+			String subSet = Integer.toBinaryString(i);
+			
+			// pad zeroes in front of subset if it's length is les than input length
+			if(subSet.length() < inputLength) {
+				subSet = padZeroes(subSet, inputLength);
+			}
+			
+			StringBuffer sb = new StringBuffer();
+			
+			for(int k = 0; k < subSet.length(); k++) {
+					
+				if(subSet.charAt(k) == '1') {
+					sb.append(input.charAt(k));
+				}
+			}
+			
+			powerSet.add(new String(sb));
+			
+		}
+		
+		return powerSet;
+	}
+	
+	/**
+	 * Recursively pad zeroes to string until it's size is equal to n
+	 * 
+	 * @param s
+	 * @param n
+	 * @return
+	 */
+	private static String padZeroes(String s, int n) {
+		
+		if(s.length() == n) {
+			return s;
+		}
+		
+		return padZeroes("0" + s, n);
+	}
+	
+	private static void printList(List<String> list) {
 		
 		if(list == null) {
 			throw new NullPointerException("List is null");
@@ -73,15 +140,19 @@ public class SubSets {
 		while(iterator.hasNext()) {
 			System.out.println("{" + iterator.next() + "}");
 		}
-	}
-
+	}	
+	
 	public static void main(String[] args) {
 		
-		String text = "abc";
+		String text = "abcd";
 		
 		List<String> subsets = findAllSubSets(text);
-		
+		System.out.println("Powerset using recursive method:");
 		printList(subsets);
+	
+		List<String> powerSet = powerSetUsingCombinatorics(text);	
+		System.out.println("\n\nPowerset using combinatorics method:");
+		printList(powerSet);
 
 	}
 
