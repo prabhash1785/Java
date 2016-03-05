b34cda800ee66356b48c10e1321c0e969b8bfc1d
Prabhash Rathore <prabhashrathore@gmail.com>
2016-03-05 11:38:19 -0800

    comparing c++ elementary data with java

---
+++ JavaCodeSnippets/src/com/prabhash/java/lang/ElementaryDataTypes.java
@@ -0,0 +1,17 @@
+package com.prabhash.java.lang;
+
+public class ElementaryDataTypes {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		System.out.println("a = " + a);
+		
+		// int b = 20.7; // this results in compilation error, i.e. cannot assign int to double type value
+		
+		char c = 90;
+		System.out.println("Char c = " + c);
+		
+	}
+
+}
