e8108d80f7a0cc2c9e704a02631515ffee0755be
Prabhash Rathore <prabhashrathore@gmail.com>
2016-04-29 06:02:36 -0700

    Interweave Linked List with constant space

--- CrackingTheCodeInterview/src/com/prabhash/interview/practice/linkedlist/InterweaveLinkedList.java
+++ CrackingTheCodeInterview/src/com/prabhash/interview/practice/linkedlist/InterweaveLinkedList.java
@@ -57,6 +57,17 @@ public class InterweaveLinkedList {
 		return head;
 	}
 	
+	/**
+	 * Method 2:
+	 * Interweave a singly Linked List with Space optimization. In this case, we are just storing half of the Linked List nodes in
+	 * Stack as compared to Method 1 where we stored all the nodes
+	 * 
+	 * Time Complexity: O(n)
+	 * Space Complexity: O(n)
+	 * 
+	 * @param head
+	 * @return head
+	 */
 	public static Node interweaveUsingStackOptimizedForSpace(Node head) {
 		
 		if(head == null) {
@@ -94,6 +105,83 @@ public class InterweaveLinkedList {
 		}
 		
 		return head;
+	}
+	
+	/**
+	 * Method 3:
+	 * This method further optimizes interweaving Linked List by not using a Stack. It uses constant amount of space.
+	 * 
+	 * This method first finds the mid point in the Linked List and then it reverses the Linked List from mid point to end. Once 
+	 * second half of Linked List is reversed then it interweaves the Linked List by following same steps as previous methods.
+	 * 
+	 * Time Complexity: O(n)
+	 * Space Complexity: O(1)
+	 * 
+	 * @param head
+	 * @return
+	 */
+	public static Node interweaveLLUsingConstantSpace(Node head) {
+		
+		if(head == null) {
+			throw new IllegalArgumentException("Linked List is null");
+		}
+		
+		Node fast = head;
+		Node slow = head;
+		
+		// find the mid point node of Linked List
+		while(fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		
+		// reverse Linked List from mid point to end
+		Node end = reverseLinkedList(slow);
+		Node first = head;
+		
+		while(first != null && end != null && first != end) {
+			
+			Node tempFirst = first.next;
+			Node tempEnd = end.next;
+			
+			first.next = end;
+			end.next = tempFirst;
+			
+			first = tempFirst;
+			end = tempEnd;
+		}
+		
+		return head;
+	}
+	
+	private static Node reverseLinkedList(Node node) {
+		
+		if(node == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		Node prev = node;
+		Node current = node.next;
+		Node future = null;
+		
+		if(current != null) {
+			future = current.next;
+		}
+		
+		prev.next = null; // reset prev's next pointer to null to prevent cycles
+		
+		while(future != null) {
+			
+			current.next = prev;
+			prev = current;
+			current = future;
+			
+			future = future.next;
+		}
+		
+		current.next = prev;
+		
+		return current;
 	}
 	
 	private static void printLinkedList(Node head) {
@@ -135,6 +223,10 @@ public class InterweaveLinkedList {
 		
 		head = interweaveUsingStackOptimizedForSpace(head);
 		System.out.println("\nInterweaved LinkedList using Space Optimized Algorithm:");
+		printLinkedList(head);
+		
+		head = interweaveLLUsingConstantSpace(head);
+		System.out.println("\nInterweaved Linked List using constant space:");
 		printLinkedList(head);
 	}
 }
