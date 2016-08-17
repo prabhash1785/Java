1fc481052b87cedbc89510c6d0c259073b943ade
Prabhash Rathore <prabhashrathore@gmail.com>
2016-08-17 07:42:11 -0700

    Merge array and remove duplicates using linkedhashset

--- CrackingTheCodeInterview/src/com/prabhash/interview/practice/sort/MergeTwoSortedArraysAndRemoveDuplicates.java
+++ CrackingTheCodeInterview/src/com/prabhash/interview/practice/sort/MergeTwoSortedArraysAndRemoveDuplicates.java
@@ -1,5 +1,9 @@
 package com.prabhash.interview.practice.sort;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
  * Merge given two sorted arrays. The resulting array should not have any duplicates.
  * 
@@ -71,6 +75,56 @@ public class MergeTwoSortedArraysAndRemoveDuplicates {
 		
 		return c;
 	}
+	
+	/**
+	 * Method 2:
+	 * Use a LinkedHashSet data structure property to merge two given sorted arrays and eliminate duplicates from being added to the set.
+	 * 
+	 * Time Complexity: O(m + n)
+	 * Space Complexity: O(m + n)
+	 * 
+	 * @param a
+	 * @param b
+	 * @return c
+	 */
+	public static final int[] mergeArraysAndRemoveDuplicatesUsingSet(int[] a, int[] b) {
+		if(a == null || b == null) {
+			throw new NullPointerException();
+		}
+		
+		// This data structure will maintain order and eliminate duplicates
+		Set<Integer> set = new LinkedHashSet<>();
+		int i = 0, j = 0;
+		while(i < a.length && j < b.length) {
+			if(a[i] < b[j]) {
+				set.add(a[i]);
+				++i;
+			} else {
+				set.add(b[j]);
+				++j;
+			}
+		}
+		
+		while(i < a.length) {
+			set.add(a[i]);
+			++i;
+		}
+		
+		while(j < b.length) {
+			set.add(b[j]);
+			++j;
+		}
+		
+		// Move elements from set to an array
+		int[] c = new int[set.size()];
+		int k = 0;
+		Iterator<Integer> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			c[k++] = iterator.next();
+		}
+		
+		return c;
+	}
 
 	public static void main(String[] args) {
 		final int[] a = new int[] {2, 2, 6, 8, 9, 12, 15};
@@ -79,6 +133,12 @@ public class MergeTwoSortedArraysAndRemoveDuplicates {
 		int[] c = mergeArraysAndRemoveDuplicates(a, b);
 		System.out.println("Merged arrays:");
 		for(int i : c) {
+			System.out.print(i + " ");
+		}
+		
+		int[] d = mergeArraysAndRemoveDuplicatesUsingSet(a, b);
+		System.out.println("\nMerged arrays using set:");
+		for(int i : d) {
 			System.out.print(i + " ");
 		}
 	}
