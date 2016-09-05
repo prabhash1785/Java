d4bf9944da13d1e899f95b9e32aeb630d7a7c5f8
Prabhash Rathore <prabhashrathore@gmail.com>
2016-09-05 07:02:50 -0700

    Optimized way to find longest subset with consecutive numbers

--- CrackingTheCodeInterview/src/com/prabhash/interview/practice/array/FindLongestSubSetWithConsecutiveNumbers.java
+++ CrackingTheCodeInterview/src/com/prabhash/interview/practice/array/FindLongestSubSetWithConsecutiveNumbers.java
@@ -1,6 +1,8 @@
 package com.prabhash.interview.practice.array;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Find longest subset with consecutive numbers.
@@ -40,10 +42,61 @@ public class FindLongestSubSetWithConsecutiveNumbers {
 		
 		return maxLength;
 	}
+	
+	/**
+	 * Optimized method to find longest subset of consecutive numbers.
+	 * 
+	 * Store all numbers in a set. While set is not empty, remove one number from set and find all consecutive numbers in set both which are
+	 * lower and higher. Make sure to remove numbers once they are found. Keep updating maxCount if you find a subset longer than maxCount.
+	 * 
+	 * Time Complexity: O(n)
+	 * Space Complexity: O(n)
+	 * 
+	 * @param a
+	 * @return maxCount
+	 */
+	public static int findLongestSubSetLengthWithConsecutiveNumbersOptimized(int[] a) {
+		if(a == null) {
+			return 0;
+		}
+		
+		Set<Integer> set = new HashSet<>();
+		for(int i = 0; i < a.length; i++) {
+			set.add(a[i]);
+		}
+		
+		int maxCount = 0;
+		while(!set.isEmpty()) {
+			int num = set.iterator().next();
+			set.remove(num);
+			
+			int count = 1;
+			int lowerNum = num - 1;
+			int higherNum = num  + 1;
+			while(set.contains(lowerNum)) {
+				++count;
+				set.remove(lowerNum);
+				--lowerNum;
+			}
+			
+			while(set.contains(higherNum)) {
+				++count;
+				set.remove(higherNum);
+				++higherNum;
+			}
+			
+			if(count > maxCount) {
+				maxCount = count;
+			}
+		}
+		return maxCount;
+	}
 
 	public static void main(String[] args) {
 		int[] a = new int[] {3, -2, 7, 9, 8, 1, 2, 0, -1, 5, 8};
 		System.out.println("Largest subset length with consecutive numbers: " + getSizeOfLongestConsecutiveSubset(a));
+		
+		System.out.println("Largest subset length: " + findLongestSubSetLengthWithConsecutiveNumbersOptimized(a));
 	}
 
 }
