b140f0b23245e860940607a3cd498d01b52eb05d
Prabhash Rathore <prabhashrathore@gmail.com>
2016-09-19 20:59:03 -0700

    removed unused import

--- JavaCodeSnippets/src/com/prabhash/java/streams/BasicStreamsDemo.java
+++ JavaCodeSnippets/src/com/prabhash/java/streams/BasicStreamsDemo.java
@@ -1,7 +1,6 @@
 package com.prabhash.java.streams;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
