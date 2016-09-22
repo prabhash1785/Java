3a10cb72213d37e82bcb0a35524bd1971d73563d
Prabhash Rathore <prabhashrathore@gmail.com>
2016-09-22 10:14:09 -0700

    use predicate to test string length

--- JavaCodeSnippets/src/com/prabhash/java/lambda/HeyLambda.java
+++ JavaCodeSnippets/src/com/prabhash/java/lambda/HeyLambda.java
@@ -1,5 +1,9 @@
 package com.prabhash.java.lambda;
 
+import java.util.function.Predicate;
+
+import static org.junit.Assert.*;
+
 /**
  * Lambda Expression Play-Ground
  * 
@@ -24,5 +28,8 @@ public class HeyLambda {
 	public static void main(String[] args) {
 		Animal animal = () -> "hey there"; // Implementation of Interface using Lambda Expression
 		printAnimalSpeech(animal);
+		
+		Predicate<String> isEvenLengthString = s -> s.length() % 2 == 0;
+		assertTrue("Not an even length string", isEvenLengthString.test("four"));
 	}
 }
