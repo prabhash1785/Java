71912bccaf9a06ab4cc65dd708b3145710d1d8da
Prabhash Rathore <prabhashrathore@gmail.com>
2016-09-29 00:06:31 -0700

    Generate all possible BSTs which can be generated from a given number n

---
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/recursion/GenerateAllBSTsFrom.java
@@ -0,0 +1,72 @@
+package com.prabhash.java.interview.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateAllBSTsFrom {
+	
+	public static List<Node> getAllBSTs(int n) {
+		if(n <= 0) {
+			return null;
+		}
+		
+		return getAllBSTsHelper(1, n);
+	}
+	
+	private static List<Node> getAllBSTsHelper(int start, int end) {
+		List<Node> list = new ArrayList<>();
+		
+		if(start > end) {
+			list.add(null);
+			return list;
+		}
+		
+		for(int i = start; i <= end; i++) {
+			List<Node> leftSubTrees = getAllBSTsHelper(start, i - 1);
+			List<Node> rightSubTrees = getAllBSTsHelper(i + 1, end);
+			
+			for(int j = 0; j < leftSubTrees.size(); j++) {
+				Node node = new Node(i);
+				node.left = leftSubTrees.get(j);
+				
+				for(int k = 0; k < rightSubTrees.size(); k++) {
+					node.right = rightSubTrees.get(k);
+					list.add(node);
+				}
+			}
+		}
+		
+		return list;
+	}
+	
+	public static class Node {
+		private int data;
+		private Node left;
+		private Node right;
+		
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+	
+	public static void inOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		
+		inOrder(node.left);
+		System.out.print(node.data + " ");
+		inOrder(node.right);
+	}
+
+	public static void main(String[] args) {
+		int n = 3;
+		List<Node> list = getAllBSTs(n);
+		
+		System.out.println("List of BSTs:");
+		for(Node node : list) {
+			inOrder(node);
+			System.out.print("\n");
+		}
+	}
+}
