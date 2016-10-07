e6b85821095714f0f47aabbb4bea91ef99507bf9
Prabhash Rathore <prabhashrathore@gmail.com>
2016-10-07 05:33:45 -0700

    code clean up

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/PathFromRootToEachLeaf.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/ch4/PathFromRootToEachLeaf.java
@@ -36,15 +36,6 @@ public class PathFromRootToEachLeaf {
 			
 	}
 	
-	/**
-	 * DFS Traversal of Tree iteratively using a Stack.
-	 * 
-	 * @param list
-	 * @param count
-	 */
-	
-	
-	
 	private static void printArray(int[] list, int count) {
 		
 		for(int i = 0; i < count; i++) {
