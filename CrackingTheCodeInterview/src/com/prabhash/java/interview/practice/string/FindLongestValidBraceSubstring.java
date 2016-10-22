c769922318d61f58b4a2f363a251a2a7dd95947f
Prabhash Rathore <prabhashrathore@gmail.com>
2016-10-22 13:04:43 -0700

    Optimized method to find the longest valid brace substring

--- CrackingTheCodeInterview/src/com/prabhash/java/interview/practice/string/FindLongestValidBraceSubstring.java
+++ CrackingTheCodeInterview/src/com/prabhash/java/interview/practice/string/FindLongestValidBraceSubstring.java
@@ -52,7 +52,90 @@ public class FindLongestValidBraceSubstring {
 	
 		return longestString;
 	}
-
+	
+	/**
+	 * Find longest brace string length in linear time.
+	 * 
+	 * Every time you see an open brace, increase open brace count. For close brace, if openBrace count > 0, then increase length by 2 as
+	 * each brace comprises of 2 braces. If openbrace count < 0, then this must not be a valid brace substring so reset validlength and
+	 * openbrace count. At the end of each iteration, check if validLength > maxValidLength, if it is then update value of maxValidLength.
+	 * 
+	 * Time Complexity: O(n)
+	 * 
+	 * @param s
+	 * @return maxValidLength
+	 */
+	public static int findLongestValidBraceSubStringLengthOptimized(String s) {
+		if(s == null || s.length() < 2) {
+			return 0;
+		}
+		
+		int validLength = 0;
+		int openParenthesisCount = 0;
+		int maxValidLength = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			char parenthesis = s.charAt(i);
+			if(parenthesis == '(') {
+				openParenthesisCount++;
+			} else {
+				if(openParenthesisCount > 0) {
+					openParenthesisCount--;
+					validLength = validLength + 2;
+				} else {
+					validLength = 0;
+					openParenthesisCount = 0;
+				}
+			}
+			if(maxValidLength < validLength) {
+				maxValidLength = validLength;
+			}
+		}
+		return maxValidLength;
+	}
+	
+	/**
+	 * Optimized method to find longest valid brace substring.
+	 * 
+	 * Every time you see an open brace, increase open brace count. For close brace, if openBrace count > 0, then increase length by 2 as
+	 * each brace comprises of 2 braces. If openbrace count < 0, then this must not be a valid brace substring so reset validlength and
+	 * openbrace count. At the end of each iteration, check if validLength > maxValidLength, if it is then find substring from (i + 1 -
+	 * validLength) to (i + 1)th position and update maxValidString value.
+	 * 
+	 * Time Complexity: O(n)
+	 * 
+	 * @param s
+	 * @return maxValidString
+	 */
+	public static String findLongestValidBraceSubStringOptimized(String s) {
+		if(s == null || s.length() < 2) {
+			return "";
+		}
+		
+		int validLength = 0;
+		int openParenthesisCount = 0;
+		String maxValidString = "";
+		
+		for (int i = 0; i < s.length(); i++) {
+			char parenthesis = s.charAt(i);
+			if(parenthesis == '(') {
+				openParenthesisCount++;
+			} else {
+				if(openParenthesisCount > 0) {
+					openParenthesisCount--;
+					validLength += 2;
+				} else {
+					validLength = 0;
+					openParenthesisCount = 0;
+				}
+			}
+			if(maxValidString.length() < validLength) {
+				maxValidString = s.substring(i + 1 - validLength, i + 1);
+			}
+		}
+		return maxValidString;
+	}
+	
 	public static void main(String[] args) {
 		String input1 = "(()()";
 		String longestSubstring = findLongestValidBraceString(input1);
@@ -61,5 +144,13 @@ public class FindLongestValidBraceSubstring {
 		String input2 = ")))()()(())(((";
 		String longestSub2tring2 = findLongestValidBraceString(input2);
 		System.out.println("Longest substring: " + longestSub2tring2);
+		
+		// Method 2 test cases
+		System.out.println("Longest Brace string for " + input1 + ": " + findLongestValidBraceSubStringLengthOptimized(input1));
+		System.out.println("Longest Brace string for " + input2 + ": " + findLongestValidBraceSubStringLengthOptimized(input2));
+		
+		// Method 3 test cases
+		System.out.println("Longest Brace string for " + input1 + ": " + findLongestValidBraceSubStringOptimized(input1));
+		System.out.println("Longest Brace string for " + input2 + ": " + findLongestValidBraceSubStringOptimized(input2));
 	}
 }
