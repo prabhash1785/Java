d1a87aaaf1e32c92c931492c2720cf9871287541
Prabhash Rathore <prabhashrathore@gmail.com>
2016-10-23 12:50:03 -0700

    server and client communication using socket

--- JavaCodeSnippets/src/com/prabhash/java/net/ClientSocket.java
+++ JavaCodeSnippets/src/com/prabhash/java/net/ClientSocket.java
@@ -1,6 +1,8 @@
 package com.prabhash.java.net;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 /**
@@ -14,9 +16,24 @@ public class ClientSocket {
 	public static void main(String[] args) throws IOException {
 		
 		Socket clientSocket = new Socket("localhost", 9000);
+		System.out.println("Opened socket connection with server..");
 		
+		OutputStream outputStream = clientSocket.getOutputStream();
+		outputStream.write("Hello Server\n".getBytes());
+		outputStream.flush();
+		
+		InputStream inputStream = clientSocket.getInputStream();
+		byte[] data = new byte[10];
+		System.out.print("Server said:: ");
+		int dataRead = inputStream.read(data);
+		while(dataRead > 0) {
+			System.out.print(new String(data));
+			dataRead = inputStream.read(data);
+		}
+		
+		outputStream.close();
+		inputStream.close();
 		clientSocket.close();
-
 	}
 
 }
--- JavaCodeSnippets/src/com/prabhash/java/net/MyServerSocket.java
+++ JavaCodeSnippets/src/com/prabhash/java/net/MyServerSocket.java
@@ -1,44 +1,103 @@
 package com.prabhash.java.net;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Server Socket which will count the number of client connections received
  * 
- * @author prrathore
+ * @author Prabhash Rathore
  *
  */
 public class MyServerSocket {
 	
 	private static int connectionCount = 0;
+	private static final int PORT = 9000; 
+	private static Map<String, String> dataStore;
+	
+	static {
+		dataStore = new HashMap<>();
+		dataStore.put("foo", "bar");
+		dataStore.put("apple", "mango");
+		dataStore.put("US", "India");
+		dataStore.put("Earth", "Sun");
+	}
 
 	public static void main(String[] args) throws IOException {
-		
-		int serverPort = 9000;
-		ServerSocket server = new ServerSocket(serverPort);
-		
-		System.out.println("Server is running at port: " + serverPort);
+		ServerSocket server = new ServerSocket(PORT);
+		System.out.println("Server is running at port: " + PORT);
 		
 		boolean isStopped = false;
 		while(!isStopped){
 		    Socket clientSocket = server.accept();
 		    
-		    connectionCount++; //increment Client Connection when new connection received
-		    
-		    System.out.println("Number of client connection received # " + connectionCount);
-		    
-		    clientSocket.close(); //close client connection
-		    
+		    connectionCount++; //increment Client Connection when new connection received 
+		    System.out.println("\nNumber of client connection received # " + connectionCount);
+		   
+		    // delegate client socket to a new worker thread
+		    WorkerThread workerThread = new WorkerThread(clientSocket, connectionCount);
+		    workerThread.start();
 		}
 		
 		System.out.println("Server is going to stop!!");
-		
 		server.close(); //close server socket
-		
 		System.out.println("Server stopped!");
+	}
+	
+	/**
+	 * Worker Thread spawned by Server for each client connection.
+	 * 
+	 * @author Prabhash Rathore
+	 *
+	 */
+	static class WorkerThread extends Thread {
+		private Socket socket;
+		private int connectionID;
 		
+		public WorkerThread(Socket socket, int connectionID) {
+			this.socket = socket;
+			this.connectionID = connectionID;
+		}
+		
+		@Override
+		public void run() {
+			BufferedReader bufferedReader = null;
+			OutputStream outputStream = null;
+			try {
+				bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				System.out.print("\nClient said:: ");
+				String line = bufferedReader.readLine();
+				while(line != null) {
+					System.out.print(line);
+					line = bufferedReader.readLine();
+				}
+				
+				// Reply to client
+				outputStream = socket.getOutputStream();
+				String message = "Hello Client at Connection ID: " + connectionID;
+				outputStream.write(message.getBytes());
+				outputStream.flush();
+			} catch(IOException ioException) {
+				ioException.printStackTrace();
+			} finally {
+				System.out.println("\nGoing to close all streams and socket for clinet at connection: " + connectionID);
+				
+				try {
+					bufferedReader.close();
+					outputStream.close();
+					socket.close();
+				} catch(IOException ioException) {
+					ioException.printStackTrace();
+				} 
+			}
+		}
 	}
 
 }
